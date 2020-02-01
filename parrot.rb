4bd7a4de12ad33ae65ed09ce5b9bb207e7bb1adf
Cristian Pintado <cpintado17@gmail.com>
2020-02-01 19:05:01 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot("Squawk!")
-  the_parrot = "Squawk!"
+def parrot("prhase")
+  phrase = "Squawk!"
   return "Squawk!"
   "Squawk!"
 end
